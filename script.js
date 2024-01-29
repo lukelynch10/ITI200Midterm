fd61df6e7c91c338fb336441e5603c34482682a9
llynch10 <luke.s.lynch@gmail.com>
2024-01-29 18:10:34 -0500

    modified:   index.html 	modified:   script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+"use strict";
+//variables
+let characterName = "";
+let healthPoints = 100;
+
+//random number generator 
+function randomNum(params) {
+    const randomDecimal = Math.random();
+    const randomNumber = Math.floor(randomDecimal * 10) + 1;
+}
