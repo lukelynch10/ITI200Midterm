5832e50dc0a3baf0a32a3ee35d49b9b0b1bf5a70
lukelynch10 <luke.s.lynch@gmail.com>
2024-02-13 22:46:58 -0500

    very basic gameplay functionality

--- script.js
+++ script.js
@@ -24,83 +24,83 @@ function battleDamage() {
 function scenarios(randomNum){
     switch(randomNum) {
         case 1:
-          scenario()
+          
           console.log("1")
         break;
         case 2:
-          scenario()
+          
             console.log("2")
         break;
         case 3:
-          scenario()
+          
           console.log("3")  
         break;
         case 4:
-          scenario()
+          
           console.log("4")  
         break;
         case 5:
-          scenario()
+          
           console.log("5")  
         break;
         case 6:
-          scenario()
+          
           console.log("6")  
         break;
         case 7:
-          scenario()
+          
           console.log("7")  
         break;
         case 8:
-          scenario()
+          
           console.log("8")  
         break;
         case 9:
-          scenario()
+         
           console.log("9")  
         break;
         case 10:
-          scenario()
+          
           console.log("10")  
         break;
         case 11:
-          scenario()
+          
           console.log("11")  
         break;
         case 12:
-          scenario()
+          
           console.log("12")  
         break;
         case 13:
-          scenario()
+          
           console.log("13")  
         break;
         case 14:
-          scenario()
+          
           console.log("14")  
         break;
         case 15:
-          scenario()
+          
           console.log("15")  
         break;
         case 16:
-          scenario()
+          
           console.log("16")  
         break;
         case 17:
-          scenario()
+          
           console.log("17")  
         break;
         case 18:
-          scenario()
+          
           console.log("18")  
         break;
         case 19:
-          scenario()
+          
           console.log("19")  
         break;
         case 20:
-          scenario()
+          
           console.log("20")  
         break;
         default:
@@ -115,43 +115,37 @@ $("#fight").hide();
 $("#run").hide();
 $("#playerStatus").hide();
 
-function scenario(){
-  $("#fight").show();
-$("#run").show();
-$("#Continue").hide();
-}
-
 $(function(){
   $("#playnow").click(function(){
     scenarios(randomNum());
-    
     $("#playnow").hide();
+    $("#fight").show();
+    $("#run").show();
+    $("#playerStatus").show();
     turnCounter++;
     })
   });
 
-  $(function(){
-    $("#Continue").click(function(){
-      scenarios(randomNum());
-      turnCounter++;
-      // end game when turns reach zero
-      if (turnCounter === 10 || healthPoints === 0){
-        $("#Continue").hide();
-        $("#fight").hide();
-        $("#run").hide();
-      };  
-    })
-    });
+  // $(function(){
+  //   $("#Continue").click(function(){
+  //     scenarios(randomNum());
+  //     turnCounter++;
+  //     // end game when turns reach zero
+  //     if (turnCounter === 10 || healthPoints === 0){
+  //       $("#Continue").hide();
+  //       $("#fight").hide();
+  //       $("#run").hide();
+  //     };  
+  //   })
+  //   });
 
     $(function(){
       $("#fight").click(function(){
         scenarios(randomNum());
-        $("#Continue").show();
-        $("#fight").hide;
-        $("#run").hide;
-        healthPoints -= battleDamage();
-        console.log(healthPoints)
-        if (turnCounter === 10 || healthPoints === 0){
+        healthPoints -= damageMultiplier * battleDamage();
+        $("#healthText").text(healthPoints);
+        turnCounter++;
+        if (turnCounter === 10 || healthPoints <= 0){
           $("#Continue").hide();
           $("#fight").hide();
           $("#run").hide();
@@ -162,10 +156,13 @@ $(function(){
       $(function(){
         $("#run").click(function(){
           scenarios(randomNum());
-          $("#Continue").show();
-          $("#fight").hide;
-          $("#run").hide;
-          
+          damageMultiplier *= 2;
+          turnCounter++;
+        if (turnCounter === 10 || healthPoints <= 0){
+          $("#Continue").hide();
+          $("#fight").hide();
+          $("#run").hide();
+        }; 
           })
         });
 
